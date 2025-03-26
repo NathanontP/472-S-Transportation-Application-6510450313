c6e7d31470aef0d0dcec1c9ca3bfc7bdd9ae6222
NathanontP <thezeronxd002@gmail.com>
2025-03-26 15:38:09 +0700

    fix typo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,24 +3,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: '0.0.0.0', // เปิดให้เข้าถึงจากภายนอก
-    port: 4173, // ใช้พอร์ต 4173
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 4173, // ✅ เปลี่ยนพอร์ตเป็น 4173
-    allowedHosts: ['g1-472.jgogo01.in.th'], // ✅ อนุญาตให้เข้าถึงจาก domain นี้
-  },
-  define: {
-    'process.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || )
-  }
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    server: {
+        host: '0.0.0.0', // เปิดให้เข้าถึงจากภายนอก
+        port: 4173, // ใช้พอร์ต 4173
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: 4173, // ✅ เปลี่ยนพอร์ตเป็น 4173
+        allowedHosts: ['g1-472.jgogo01.in.th'], // ✅ อนุญาตให้เข้าถึงจาก domain นี้
+    },
+    define: {
+        'process.env.VITE_API_BASE_URL': JSON.stringify(
+            process.env.VITE_API_BASE_URL
+        ),
+    },
 })
