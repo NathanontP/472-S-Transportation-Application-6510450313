215b16aea1a933a8376b05ad8bd4ebd6a86b38c7
NathanontP <thezeronxd002@gmail.com>
2025-03-26 14:49:23 +0700

    init: api base url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import store from './stores/store';
+import store from './stores/store'
 
 import App from './App.vue'
 import router from './router'
-
+export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
 const app = createApp(App)
 
 app.use(store)
